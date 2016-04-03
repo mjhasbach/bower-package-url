b0757ff83baace1bb0e8d2fc9039172169efa0d2
Matthew Hasbach <hasbach.git@gmail.com>
2016-04-03 13:43:53 -0400

    Add additional test cases

--- test/test.js
+++ test/test.js
@@ -1,9 +1,28 @@
-var should = require('chai').should(),
+var uuid = require('uuid'),
+    should = require('chai').should(),
     bowerPackageURL = require('../dist/bowerPackageURL');
 
 describe('bower-package-url', function() {
-    it('should get the repository URL associated with a bower package name', function(done) {
-        bowerPackageURL('lodash', function(err, url) {
+    var pkg = 'lodash';
+
+    it('should throw an error if cb is not a function', function(done) {
+        (function() {bowerPackageURL(pkg);}).should.throw();
+        done();
+    });
+    it('should call back an error if packageName is not a string', function(done) {
+        bowerPackageURL(null, function(err) {
+            err.should.be.an('error');
+            done();
+        });
+    });
+    it('should call back an error if packageName is not found on Bower', function(done) {
+        bowerPackageURL(uuid.v4(), function(err) {
+            err.should.be.an('error');
+            done();
+        });
+    });
+    it('should get the repository URL associated with a Bower package name', function(done) {
+        bowerPackageURL(pkg, function(err, url) {
             should.not.exist(err);
             url.should.equal('git://github.com/lodash/lodash.git');
             done();
